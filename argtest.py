3e71ee6c068faec44f694d3288fcb3716be8f9cc
Matthew Arnold <marnold@riverstoneschool.org>
2023-03-08 09:35:37 -0700

    Added verbose feature to pdf_breaker

--- argtest.py
+++ argtest.py
@@ -8,5 +8,10 @@ Simple test program for parsing arguments.
 
 def print_args():
     """ Simple function to print arguments """
+    counter = 0
     for arg in sys.argv[1:]:
+        counter += 1
         print(arg)
+        print(counter)
+
+print_args()
--- pdf_breaker.py
+++ pdf_breaker.py
@@ -2,12 +2,18 @@
 
 import PyPDF2
 import os, sys
+from time import sleep
 
 home_dir = os.path.expanduser('~')
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print('Usage: pdf_breaker <encrypted pdf>')
     sys.exit()
+
+if '-v' in sys.argv:
+    verbose_flag = 1
+else:
+    verbose_flag = 0
 
 # open the dictionary file
 if os.path.exists('dictionary.txt'):
@@ -22,17 +28,41 @@ for word in dict_words:
     test_words.append(word.strip())
 
 # open pdf file
-pdfReader = PyPDF2.PdfFileReader(open(sys.argv[1], 'rb'))
+for arg in sys.argv:
+    if arg[0] == '-':
+        continue
+    else:
+        pdf_file_name = arg
+#pdfReader = PyPDF2.PdfFileReader(open(sys.argv[1], 'rb'))
+pdfReader = PyPDF2.PdfFileReader(pdf_file_name, 'rb')
 
+# Print a message warning that verbose has been selected, then count down
+# to start.
+if verbose_flag == 1:
+    print('Verbose flag detected, test words will be printed as they\'re '
+          'used.'
+    )
+    sleep(0.3)
+    print('Starting in 3...')
+    sleep(1)
+    print('            2...')
+    sleep(1)
+    print('            1...')
+    sleep(1)
+
+# Start crackin'!
 if pdfReader.isEncrypted:
     for word in test_words:
         # use lower(), upper(), and title()
+        if verbose_flag == 1: print(word.lower())
         if pdfReader.decrypt(word.lower()):
             print(f'Password is {word.lower()}')
             sys.exit()
+        if verbose_flag == 1: print(word.upper())
         if pdfReader.decrypt(word.upper()):
             print(f'Password is {word.upper()}')
             sys.exit()
+        if verbose_flag == 1: print(word.title())
         if pdfReader.decrypt(word.title()):
             print(f'Password is {word.title()}')
             sys.exit()
