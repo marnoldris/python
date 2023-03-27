b0b19894a8028bbd612590712c9f57bfa7c38dfe
Matthew Arnold <marnold@riverstoneschool.org>
2023-03-27 08:52:13 -0600

    Added arguments functionality

--- random_csl_generator.py
+++ random_csl_generator.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 
-import random
+import random, sys
 import pyperclip
 
-
-n1 = int(input('\nPlease enter the lower bound: '))
-n2 = int(input('Please enter the upper bound: '))
-number_of_numbers = int(input('Please enter the desired number of numbers: '))
+if len(sys.argv) == 1:
+    n1 = int(input('\nPlease enter the lower bound: '))
+    n2 = int(input('Please enter the upper bound: '))
+    number_of_numbers = int(input('Please enter the desired number of numbers: '))
+elif len(sys.argv) == 4:
+    n1 = int(sys.argv[1])
+    n2 = int(sys.argv[2])
+    number_of_numbers = int(sys.argv[3])
+else:
+    print(f'Usage: $ {sys.argv[0]} with no arguments to interactively '
+          f'add upper and lower bounds, as well as a length for the list. '
+          f'\n\nOr $ {sys.argv[0]} <lower bound> <upper bound> <length> to '
+          f'specify bounds at run time.'
+)
+    exit()
 
 bounds = (n1, n2)
 lower = min(bounds)
