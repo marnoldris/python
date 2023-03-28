0177d2a420b1139575742e821f2d7f87f0325a16
Matthew Arnold <marnold@riverstoneschool.org>
2023-03-28 15:22:03 -0600

    Cleanup

--- random_csl_generator.py
+++ random_csl_generator.py
@@ -6,7 +6,7 @@ import pyperclip
 if len(sys.argv) == 1:
     n1 = int(input('\nPlease enter the lower bound: '))
     n2 = int(input('Please enter the upper bound: '))
-    number_of_numbers = int(input('Please enter the desired number of numbers: '))
+    number_of_numbers = int(input('Please enter the desired number of integers: '))
 elif len(sys.argv) == 4:
     n1 = int(sys.argv[1])
     n2 = int(sys.argv[2])
@@ -16,15 +16,17 @@ else:
           f'add upper and lower bounds, as well as a length for the list. '
           f'\n\nOr $ {sys.argv[0]} <lower bound> <upper bound> <length> to '
           f'specify bounds at run time.'
-)
+         )
     exit()
 
 bounds = (n1, n2)
 lower = min(bounds)
 upper = max(bounds)
 
+# Fill a list with random numbers in the given range
 random_numbers = [random.randint(lower, upper) for i in range(number_of_numbers)]
 
+# Join all of the values in the list into a single string
 output = ', '.join(str(i) for i in random_numbers)
 
 print('\n\nRandom numbers: ', end='\n\n')
---
+++ tictactoe.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+         'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+         'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
+        }
+
+def print_board(board):
+    print()
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+print('\n\n\n')
+#print_board(board)
+
+turn = 'X'
+for i in range(9):
+    print_board(board)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    board[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+print_board(board)
