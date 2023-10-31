d33191208f1c9d282ba2a693f39ff25090f3049a
marnoldris <100306063+marnoldris@users.noreply.github.com>
2023-10-30 21:52:40 -0600

    Add files via upload

--- matplots/basic_xy.py
+++ matplots/basic_xy.py
@@ -7,11 +7,68 @@ Created on Fri Oct 13 10:49:25 2023
 """
 
 import matplotlib.pyplot as plt
+import sys
 
-X_POS_LIM = 15
-X_NEG_LIM = -15
-Y_POS_LIM = 15
-Y_NEG_LIM = -15
+if len(sys.argv) == 3:
+    try:
+        X_POS_LIM = int(sys.argv[1])
+        X_NEG_LIM = -1*int(sys.argv[1])
+        Y_POS_LIM = int(sys.argv[2])
+        Y_NEG_LIM = -1*int(sys.argv[2])
+    except ValueError:
+        print('Invalid arguments, exiting...')
+        sys.exit()
+
+elif len(sys.argv) == 5:
+    try:
+        X_POS_LIM = int(sys.argv[1])
+        X_NEG_LIM = int(sys.argv[2])
+        Y_POS_LIM = int(sys.argv[3])
+        Y_NEG_LIM = int(sys.argv[4])
+    except ValueError:
+        print('Invalid arguments, exiting...')
+        sys.exit()
+    if X_POS_LIM <= X_NEG_LIM or Y_POS_LIM <= Y_NEG_LIM:
+        print('Invalid entry, the first value should be the x positive '
+              'limit, the second should be the x negative limit, '
+              'the third should be the y positive limit, and the '
+              'fourth should be the y negative limit.\n'
+              'Please try again.')
+        sys.exit()
+else:
+    while True:
+        continue_flag = False
+        print('Please enter the desired dimensions, with each value separated '
+              'by a space. These should be four values:')
+        value_string = input('> ')
+        values = value_string.split(' ')
+        int_values = []
+        for value in values:
+            try:
+                int_values.append(int(value))
+            except ValueError:
+                print('Invalid entry, please try again')
+                continue_flag = True
+                break
+        if continue_flag:
+            continue
+        try:
+            X_POS_LIM, X_NEG_LIM, Y_POS_LIM, Y_NEG_LIM = int_values
+        except ValueError:
+            print('Invalid entry (too many or too few values entered.\n'
+                  'Please try again.')
+            continue
+        if X_POS_LIM <= X_NEG_LIM or Y_POS_LIM <= Y_NEG_LIM:
+            print('Invalid entry, the first value should be the x positive '
+                  'limit, the second should be the x negative limit, '
+                  'the third should be the y positive limit, and the '
+                  'fourth should be the y negative limit.\n'
+                  'Please try again.')
+            continue
+        
+        break
+        
+
 
 fig, ax = plt.subplots(figsize=(12, 10))
 
