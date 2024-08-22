250646a21e485d29f9879e249161d63a46ba0cdd
marnoldris <100306063+marnoldris@users.noreply.github.com>
2024-08-22 08:33:32 -0600

    Add files via upload

--- xy_plot.py
+++ xy_plot.py
@@ -30,17 +30,17 @@ elif len(sys.argv) == 5:
         print('Invalid arguments, exiting...')
         sys.exit()
     if X_POS_LIM <= X_NEG_LIM or Y_POS_LIM <= Y_NEG_LIM:
-        print('Invalid entry, the first value should be the x positive '
-              'limit, the second should be the x negative limit, '
-              'the third should be the y positive limit, and the '
-              'fourth should be the y negative limit.\n'
+        print('Invalid entry, the first value should be the negative x '
+              'limit, the second should be the positive x limit, '
+              'the third should be the negative y limit, and the '
+              'fourth should be the positive y limit.\n'
               'Please try again.')
         sys.exit()
 else:
     while True:
         continue_flag = False
         print('Please enter the desired dimensions, with each value separated '
-              'by a space. These should be four values:')
+              'by a space. Ex. -10 10 -5 15:')
         try:
             value_string = input('> ')
         except KeyboardInterrupt:
@@ -64,23 +64,23 @@ else:
                   'Please try again.')
             continue
         if X_POS_LIM <= X_NEG_LIM or Y_POS_LIM <= Y_NEG_LIM:
-            print('Invalid entry, the first value should be the x negative '
-                  'limit, the second should be the x positive limit, '
-                  'the third should be the y negative limit, and the '
-                  'fourth should be the y positive limit.\n'
+            print('Invalid entry, the first value should be the negative x '
+                  'limit, the second should be the positive x limit, '
+                  'the third should be the negative y limit, and the '
+                  'fourth should be the positive y limit.\n'
                   'Please try again.')
             continue
 
         break
 
 #%% Axis labels
-print('Please enter the desired label for the x-axis:')
+print('Please enter the desired label for the x-axis. Press return to omit.')
 try:
     x_label = input('> ')
 except KeyboardInterrupt:
     print('Exiting...')
     sys.exit()
-print('Please enter the desired label for the y-axis:')
+print('Please enter the desired label for the y-axis. Press return to omit.')
 try:
     y_label = input('> ')
 except KeyboardInterrupt:
@@ -116,7 +116,11 @@ ax.set_ylabel(y_label, rotation=0, fontsize=21)
 ax.get_yaxis().set_label_coords(-0.03, 0.48)
 
 print('Would you like to include tick labels? (y/N)')
-tick_labels = input('> ')
+try:
+    tick_labels = input('> ')
+except KeyboardInterrupt:
+    print('Exiting...')
+    sys.exit()
 yes_values = ['y', 'Y', 'yes', 'Yes']
 if tick_labels not in yes_values:
     # Remove tick labels
@@ -125,7 +129,11 @@ if tick_labels not in yes_values:
 else:
     # Set the scale on the tick labels
     print('Please enter the desired scale for the x-axis ticks (default=1)')
-    x_scale_input = input('> ')
+    try:
+        x_scale_input = input('> ')
+    except KeyboardInterrupt:
+        print('Exiting...')
+        sys.exit()
     if x_scale_input == '':
         X_SCALE = 1
     else:
@@ -160,4 +168,22 @@ plt.grid(True)
 plt.axhline(y=0, color='k')
 plt.axvline(x=0, color='k')
 
-plt.show()
+print('Would you like to save the plot to a file? (y/N)?')
+try:
+    save_file = input('> ')
+except KeyboardInterrupt:
+    print('Exiting...')
+    sys.exit()
+if save_file in yes_values:
+    print('Enter a file name for the saved plot, including the file extension.'
+          ' Ex. 10x10.png')
+    try:
+        file_name = input('> ')
+    except KeyboardInterrupt:
+        print('Exiting...')
+        sys.exit()
+    print(f'Writing plot to {file_name}...')
+    plt.savefig(file_name)
+    print('Exiting...')
+else:
+    plt.show()
